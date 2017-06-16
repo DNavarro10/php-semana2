0c888af77e97881b2780a96d7c0e296e088b334a
DNavarro10 <luis_d_100@hotmail.com>
2017-06-15 21:05:44 -0500

    Estructura y nuevos archivos
    creando classe persona

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<title>Semana 1</title>
+	<link rel="stylesheet" href="css/Style.css">
+</head>
+
+<body>
+<div class="contenedor">
+		<div class="titulo">
+			<header>
+				<div><h1>Curso de PHP - Semana 2</h1></div>
+				<div><p>Programacion orientada a objetos</p></div>
+				
+			</header>
+		</div>
+		<div class="main">
+			<div class="php">
+				<?php
+				echo "Este es un ejemplo de un documento web que muestra este texto ";
+				echo "en el cuerpo de la p&aacute;gina web,";
+				echo "si puedes leer estas palabras en su navegador,";
+				echo "hizo el ejercicio bien";
+				?>
+			</div>
+		</div>
+		<footer>
+			<div>
+				<p>
+					Diego Navarro - PHP 2017
+				</p>
+			</div>
+		</footer>
+	</div>
+</body>
+
+</html>
---
+++ php/persona.php
@@ -0,0 +1,21 @@
+<?php 
+	class persona {
+		//atrubutos
+		public $cedula;
+		public $nombre;
+		public $apellidos;
+		public $aniodeNacimiento;
+		public $edad;
+		
+		//constructor
+		function _construct($cedula, $nombre, $apellidos, $aniodeNacimiento , $edad){
+			$this -> cedula = $cedula;
+			$this -> nombre = $nombre;
+			$this -> apellido = $apellidos;
+			$this -> anioNacimiento $aniodeNacimiento;
+			$this -> edad = $edad;
+				
+		}
+	}
+
+?>
